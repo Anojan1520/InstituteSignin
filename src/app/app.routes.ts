fbbf72fafde8a021d2898d11712cdee0565f6382
Anojan1520 <unicomtic1520@gmail.com>
2024-11-07 12:35:15 +0530

    Update Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Home/login/login.component';
+import { SigninComponent } from './auth/signin/signin.component';
 
 export const routes: Routes = [
-    {path:'',component:LoginComponent}
+    {path:'',component:LoginComponent},
+    {path:'signin',component:SigninComponent}
 ];
